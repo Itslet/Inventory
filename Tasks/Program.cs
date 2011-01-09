a6f69627ce5ef99cfcbb462b3ff76e581d0e25e9
Jacqueline <jacq@itslet.nl>
2011-01-09 23:01:54 +0100

    another minor thingy

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -17,19 +17,25 @@ namespace Tasks
                 s.DeleteAll<BladeServer>();
                 s.DeleteAll<BladeChassis>();
 
-                var bc = new BladeChassis { ChassisID = "1", UnitsHigh = 8 };
+                var bc = new BladeChassis { ChassisID = "1", Description="HP BladeSystem 7000c", UnitsHigh = 8, Bays = 8, Power="2400W"};
+                var bc1 = new BladeChassis { ChassisID = "2", Description = "HP BladeSystem 300c", UnitsHigh = 4, Bays = 6, Power = "2400W" };
 
                 var bs = new BladeServer { Hostname = "SRV01", IPAddress = "10.10.10.12", OS = "Server 2008 R2", ServerRole = "Domain Controller", BladeChassis = bc };
 
                 var bs1 = new BladeServer { Hostname = "SRV02", IPAddress = "10.10.10.11", OS = "Server 2008 R2", ServerRole = "Exchange 2010", BladeChassis = bc };
 
+                var bs2 = new BladeServer { Hostname = "SRV03", IPAddress = "10.10.10.13", OS = "Server 2008 R2", ServerRole = "SQL 2008", BladeChassis = bc1 };
+
                 List<BladeServer> servers = new List<BladeServer>();
                 servers.Add(bs);
                 servers.Add(bs1);
+                servers.Add(bs2);
 
                 bc.Servers = servers;
                 
                 s.Save(bc);
+                s.Save(bc1);
+                
                 s.CommitChanges();
 
                 var server = s.Single<BladeServer>(x => x.Hostname == "SRV02");
